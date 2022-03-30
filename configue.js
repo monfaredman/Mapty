5c9cad814b2e1021d277aace5b90286fcb2112ac
monfaredman <monfaredman@outlook.com>
2022-03-30 18:10:09 +0430

    add leaflet

--- configue.js
+++ configue.js
@@ -1,1 +1,14 @@
-console.log(25);
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+var map = L.map("app").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([51.5, -0.09])
+  .addTo(map)
+  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+  .openPopup();
